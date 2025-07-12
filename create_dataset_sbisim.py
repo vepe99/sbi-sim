42cba0e3e42e36a9dc8334726170b87a0caddb51
vepe99 <viterbogiuseppe1999@libero.it>
2025-07-12 13:44:19 +0200

    convert the parameters to physical units and take log of masses

--- create_dataset_sbisim.py
+++ create_dataset_sbisim.py
@@ -5,6 +5,9 @@ from tqdm import tqdm
 import jax
 import jax.numpy as jnp 
 
+from astropy import units as u
+from odisseo.units import CodeUnits
+
 # def convert_to_csv(path_stored, path_to_save, num_simulations=1000):
 #     print('start converting all the npz files to csv')
 #     all_data_path = [os.path.join(path_stored, f) for f in sorted(os.listdir(path_stored))[:num_simulations] if f.endswith('.npz')]
@@ -35,6 +38,25 @@ import jax.numpy as jnp
 #     df_reference_posterior.to_csv(os.path.join(path_to_save, f'reference_posterior_samples.csv'), index=False, header=False)
 #     print(f'done converting all the npz files to csv in the folder {path_to_save}')
 
+
+def to_inference_parameters(theta, ):
+        """
+        convert from simulation parameters to inference parameters
+        """
+        code_length = 10.0 * u.kpc
+        code_mass = 1e4 * u.Msun
+        code_time = 3 * u.Gyr
+        code_units = CodeUnits(code_length, code_mass, G=1, unit_time = code_time )  
+
+        theta[0] = theta[0] # t_end is already in Gyr
+        theta[1] = np.log10(theta[1]).item() # Plummer mass is already in Msun
+        theta[2] = theta[2] * code_units.code_length.to(u.kpc) # Plummer a
+        theta[3] = np.log10(theta[3] * code_units.code_mass.to(u.Msun)).item()  # NFW Mvir
+        theta[4] = theta[4] * code_units.code_length.to(u.kpc)  # NFW r_s
+        theta[5] = np.log10(theta[5] * code_units.code_mass.to(u.Msun)).item() # MN M
+        theta[6] = theta[6] * code_units.code_length.to(u.kpc) # MN a
+        return theta
+
 def convert_to_single_npy(path_stored, path_to_save, num_simulations=1000):
     print('start converting all the npz files to a single npy file')
     all_data_path = [os.path.join(path_stored, f) for f in sorted(os.listdir(path_stored))[:num_simulations] if f.endswith('.npz') and 'file' in f]
@@ -47,9 +69,9 @@ def convert_to_single_npy(path_stored, path_to_save, num_simulations=1000):
     for file_path in tqdm(all_data_path):
         data = np.load(file_path)
         x.append(data['x'][:1000]) #histogram
-        theta.append(data['theta'][[0, 1, 3, 5]]) # we keep only the total integration time, the mass of Plummer, NFW and MN
-        observation.append(data['x'])
-        true_theta.append(data['theta'])
+        theta.append(to_inference_parameters(data['theta'])[[0, 1, 3, 5]]) # we keep only the total integration time, the mass of Plummer, NFW and MN
+        observation.append(data['x'][:1000])
+        true_theta.append(to_inference_parameters(data['theta'])[[0, 1, 3, 5]])
         reference_posterior.append([])
     print('x shape:', np.array(x).shape)
     print('theta shape:', np.array(theta).shape)
@@ -78,7 +100,7 @@ def num_observation_npy(path_stored, path_to_save, simulation_observation_index=
             os.makedirs(path_to_save_num_observation)
         data = np.load(file_path)
         x.append(data['x'][:1000]) #histogram
-        theta.append(data['theta'][[0, 1, 3, 5]]) # we keep only the total integration time, the mass of Plummer, NFW and MN
+        theta.append(to_inference_parameters(data['theta'])[[0, 1, 3, 5]]) # we keep only the total integration time, the mass of Plummer, NFW and MN
 
         np.save(os.path.join(path_to_save_num_observation, f'observation.npy'), x)
         np.save(os.path.join(path_to_save_num_observation, f'true_parameters.npy'), theta)
